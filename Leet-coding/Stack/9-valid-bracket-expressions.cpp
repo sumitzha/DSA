575b1ff30610fc00d4f04606763b7b5fa3b2c4a3
Sumit Kumar Jha <124793539+sumitzha@users.noreply.github.com>
2023-10-06 14:53:30 +0000

    using STACK to find min cost

--- Leet-coding/Stack/9-valid-bracket-expressions.cpp
+++ Leet-coding/Stack/9-valid-bracket-expressions.cpp
@@ -19,7 +19,7 @@ int findMinimumCost(string str)
             open++;
         else
         {
-            // removing from stack 
+            // removing from stack
             if (open >= 1)
                 open--;
             else
@@ -27,6 +27,51 @@ int findMinimumCost(string str)
         }
     }
     return ((open + 1) / 2 + (close + 1) / 2);
+}
+// same thing using stack data-structure
+int findMinimumCost(string str)
+{
+    int len = str.length();
+    if (len % 2 != 0)
+        return -1;
+
+    stack<char> st;
+    for (int i = 0; i < len; i++)
+    {
+        // if opening -> add to stack 
+        if (str[i] == '{')
+            st.push(str[i]);
+
+        else
+        {
+            // remove the valid expression part
+            if (!st.empty() && st.top() == '{')
+                st.pop();
+
+            // we add closing brackets
+            else
+                st.push(str[i]);
+            // after adding closing brackets-> invalid
+        }
+    }
+
+    int opening = 0, closing = 0;
+    while (!st.empty())
+    {
+        // count opening and closings
+        if (st.top() == '{')
+        {
+            opening++;
+            st.pop();
+        }
+        else
+        {
+            closing++;
+            st.pop();
+        }
+    }
+    int res = (opening + 1) / 2 + (closing + 1) / 2;
+    return res;
 }
 int main()
 {
